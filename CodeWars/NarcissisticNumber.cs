8adf69162bdfcffd3c19b1c1c7601422cb1b5fe1
Marcel Walk <walk.marcel.97@googlemail.com>
2020-07-27 16:03:55 +0200

    Added Kata class and test

---
+++ CodeWars/NarcissisticNumber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars
+{
+
+    /*
+    EXPLANATION
+
+    A Narcissistic Number is a positive number which is the sum of its own digits, 
+    each raised to the power of the number of digits in a given base. In this Kata, 
+    we will restrict ourselves to decimal (base 10).
+    */
+
+    /*
+    TASK
+
+    Your code must return true or false depending upon whether the given number is a Narcissistic number in base 10.
+    Error checking for text strings or other invalid inputs is not required, 
+    only valid positive non-zero integers will be passed into the function.
+    */
+
+    public static class NarcissisticNumber
+    {
+        public static bool Solution(int value)
+        {
+            return false;
+        }
+    }
+}
