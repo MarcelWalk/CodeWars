2dc10e4da4f0f442aa1e7064b648b31bbbdbe1a3
Marcel Walk <walk.marcel.97@googlemail.com>
2020-07-27 15:49:45 +0200

    Added solution file

---
+++ CodeWars/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeWar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
