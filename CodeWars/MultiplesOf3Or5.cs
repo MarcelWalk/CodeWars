520ad5d26594b172a2fa75423fc79e788a0f8284
Marcel Walk <walk.marcel.97@googlemail.com>
2020-10-14 10:40:24 +0200

    MultiplesOf3Or5

---
+++ CodeWars/MultiplesOf3Or5.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars
+{
+    public class MultiplesOf3Or5
+    {
+        public static int Solution(int max)
+        {
+            int returnNumber = 0;
+
+            for (int i = 0; i < max; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                    returnNumber += i;
+            }
+
+            return returnNumber;
+        }
+    }
+}
---
+++ CodeWarsTests/MultiplesOf3Or5Tests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeWars;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars.Tests
+{
+    [TestClass()]
+    public class MultiplesOf3Or5Tests
+    {
+        [TestMethod()]
+        public void SolutionTest()
+        {
+            Assert.AreEqual(23, MultiplesOf3Or5.Solution(10));
+        }
+    }
+}
